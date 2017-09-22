765e9dca59be3e2acd68a40e90c52a9e226d1451
rsstdd <rosstod@gmail.com>
2017-09-22 09:17:15 -0700

    Refreshed node_modules, removed redux form from reducer

--- source/js/reducers/index.js
+++ source/js/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
 
 import app from 'reducers/app';
 
 export default combineReducers({
-  app,
-  form,
+  app
 });

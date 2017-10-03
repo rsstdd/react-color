6122ca41531ef229e5e5c749e2a442448ee9fd77
rsstdd <rosstod@gmail.com>
2017-10-03 15:32:20 -0700

    Config store for ssr

--- source/js/config/store.js
+++ source/js/config/store.js
@@ -2,20 +2,36 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'dev/logger';
 
+import transit from 'transit-immutable-js';
 import rootReducer from 'reducers';
 
 const isProduction = process.env.NODE_ENV === 'production';
+let INIT_STATE = null;
+
+try {
+  INIT_STATE = __MARVIN_DEHYDRATED_STATE; // eslint-disable-line no-undef
+} catch (e) {
+  console.log('Mavin: No dehydrated state'); // eslint-disable-line no-console
+}
+
+if (INIT_STATE) {
+  INIT_STATE = transit.fromJSON(INIT_STATE);
+}
 
 // Creating store
 export default () => {
   let store = null;
   let middleware = null;
-  
+
   if (isProduction) {
+    // In production adding only thunk middleware
     middleware = applyMiddleware(thunk);
   } else {
-    middleware = applyMiddleware(thunk);
-    // middleware = applyMiddleware(thunk, logger);
+    // In development mode beside thunk
+    // logger and DevTools are added
+    middleware = applyMiddleware(thunk, logger);
+
+    // Enable DevTools if browser extension is installed
     if (!process.env.SERVER_RENDER && window.__REDUX_DEVTOOLS_EXTENSION__) { // eslint-disable-line
       middleware = compose(
         middleware,
@@ -23,11 +39,21 @@ export default () => {
       );
     }
   }
-  
-  store = createStore(
-    rootReducer,
-    middleware
-  );
+
+  // Add dehydrated state if any
+  if (INIT_STATE) {
+    // Remove if you are not using server rendering
+    store = createStore(
+      rootReducer,
+      INIT_STATE,
+      middleware
+    );
+  } else {
+    store = createStore(
+      rootReducer,
+      middleware
+    );
+  }
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
